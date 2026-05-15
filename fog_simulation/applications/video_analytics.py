f53907e684fa45a9be1600500df33aedcf05a0e5
Tazana2 <danisanmez@gmail.com>
2026-05-14 21:15:05 -0500

    fix: change cpu and ram requirements for video stream processing service

--- fog_simulation/applications/video_analytics.py
+++ fog_simulation/applications/video_analytics.py
@@ -24,7 +24,7 @@ def create_video_analytics_app() -> Application:
         {"edge-video-ingestion-service":    {"Type": Application.TYPE_SOURCE}},
         {"edge-inference-service":           {"RAM": 4096,  "CPU_req": 2, "RAM_req": 4096,  "Type": Application.TYPE_MODULE}},
         {"tracking-and-event-service":       {"RAM": 3072,  "CPU_req": 1, "RAM_req": 3072,  "Type": Application.TYPE_MODULE}},
-        {"video-stream-processing-service":  {"RAM": 8192,  "CPU_req": 4, "RAM_req": 8192,  "Type": Application.TYPE_MODULE}},
+        {"video-stream-processing-service":  {"RAM": 8192,  "CPU_req": 2, "RAM_req": 4096,  "Type": Application.TYPE_MODULE}},
         {"storage-service":                  {"RAM": 16384, "CPU_req": 2, "RAM_req": 16384, "Type": Application.TYPE_MODULE}},
         {"api-and-access-service":           {"RAM": 4096,  "CPU_req": 1, "RAM_req": 4096,  "Type": Application.TYPE_MODULE}},
         {"visualization-service":            {"Type": Application.TYPE_SINK}},
